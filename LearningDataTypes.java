3eb867aa6abb6724fdb4fba62e53e061bdf8ed42
saichaitanyapetla <saichaitanya932@gmail.com>
2022-10-14 14:56:17 +0530

    my learnings 1

---
+++ LearningDataTypes.java
@@ -0,0 +1,10 @@
+
+public class LearningDataTypes {
+
+	public static void main(String[] args) {
+		System.out.println("byte size is"+Byte.SIZE);
+		System.out.println("byte max value is"+Byte.MAX_VALUE);
+
+	}
+
+}
