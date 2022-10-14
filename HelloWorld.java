35a1980c4d600f30095868915f402ec3b7940233
saichaitanyapetla <saichaitanya932@gmail.com>
2022-10-14 15:15:45 +0530

    basics

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	
+	System.out.println("hello world");
+
+	System.out.println(args[0]);
+    System.out.print(args[1]);
+ 
+	}
+
+}
